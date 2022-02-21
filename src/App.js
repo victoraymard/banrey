1dca02e001cefb8032915e1148010e26eb90a7cf
victoraymard <36413267+victoraymard@users.noreply.github.com>
2022-02-21 11:05:16 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <iframe  width="100%" height="860" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FZuFMXy59HMtn5mEywPuf2o%2FWireframe-barney%3Fpage-id%3D181%253A166%26node-id%3D213%253A160036%26viewport%3D241%252C48%252C0.81%26scaling%3Dmin-zoom%26starting-point-node-id%3D223%253A15214" allowfullscreen></iframe>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
